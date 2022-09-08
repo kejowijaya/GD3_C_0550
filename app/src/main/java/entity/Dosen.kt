112916a53b8eb221331c49ab26ccab89202d5351
kejowijaya <200710550@students.uajy.ac.id>
2022-09-08 17:14:07 +0700

    Initial commit

---
+++ app/src/main/java/entity/Dosen.kt
@@ -0,0 +1,11 @@
+package entity
+
+class Dosen(var name: String, var pengajar: String) {
+    companion object{
+        @JvmField
+        var listOfDosen = arrayOf(
+            Dosen("Fedelis Brian", "Pengajar Kelas A, B, dan D"),
+            Dosen("Thomas Adi", "Pengajar Kelas C"),
+        )
+    }
+}
